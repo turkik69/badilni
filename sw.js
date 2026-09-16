7dfde6b683ba691957e486664bbed901a114deaa
Turki alyahyai <turki.k69@gmail.com>
2026-09-16 12:51:51 +0400

    Use exchange arrows as installed app icon
    Add adaptive Android/PWA icons and an Apple touch icon based on Badilni's exchange arrows, then refresh the offline asset cache.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'badilni-v3.2';
-const ASSETS = ['./', './index.html', './firebase-store.js', './theme-v3.css', './manifest.webmanifest', './icon.svg'];
+const CACHE = 'badilni-v3.3';
+const ASSETS = ['./', './index.html', './firebase-store.js', './theme-v3.css', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png', './icon-maskable-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
